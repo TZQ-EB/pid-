53acadbc67c5812c815dc2517a5c65d80282fc07
Ton tom <128819914+TZQ-EB@users.noreply.github.com>
2023-04-20 11:44:28 +0800

    Add files via upload

---
+++ SYSTEM/usart/usart.c
@@ -0,0 +1,99 @@
+/***********************************************
+公司：轮趣科技（东莞）有限公司
+品牌：WHEELTEC
+官网：wheeltec.net
+淘宝店铺：shop114407458.taobao.com 
+速卖通: https://minibalance.aliexpress.com/store/4455017
+版本：5.7
+修改时间：2021-04-29
+
+ 
+Brand: WHEELTEC
+Website: wheeltec.net
+Taobao shop: shop114407458.taobao.com 
+Aliexpress: https://minibalance.aliexpress.com/store/4455017
+Version:5.7
+Update：2021-04-29
+
+All rights reserved
+***********************************************/
+#include "usart.h"	  
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
+#if 1
+#pragma import(__use_no_semihosting)             
+//标准库需要的支持函数                 
+struct __FILE 
+{ 
+	int handle; 
+	/* Whatever you require here. If the only file you are using is */ 
+	/* standard output using printf() for debugging, no file handling */ 
+	/* is required. */ 
+}; 
+/* FILE is typedef’ d in stdio.h. */ 
+FILE __stdout;       
+//定义_sys_exit()以避免使用半主机模式    
+_sys_exit(int x) 
+{ 
+	x = x; 
+} 
+//重定义fputc函数 
+int fputc(int ch, FILE *f)
+{      
+	if(Flag_Show==0)
+	{	
+		while((USART3->SR&0X40)==0);//Flag_Show=0  使用串口3   
+		USART3->DR = (u8) ch;      
+	}
+	else
+	{	
+		while((USART1->SR&0X40)==0);//Flag_Show!=0  使用串口1   
+		USART1->DR = (u8) ch;      
+	}	
+	return ch;
+}
+#endif 
+//end
+//////////////////////////////////////////////////////////////////
+/**************************************************************************
+Function: Serial port 1 sends data
+Input   : data：Data to be sent
+Output  : none
+函数功能：串口1发送数据
+入口参数：data：要发送的数据
+返回  值：无
+**************************************************************************/
+void usart1_send(u8 data)
+{
+	USART1->DR = data;
+	while((USART1->SR&0x40)==0);	
+}
+/**************************************************************************
+Function: Serial port 1 initialization
+Input   : pclk2：clock frequency；bound：Baud rate
+Output  : none
+函数功能：串口1初始化
+入口参数：pclk2：时钟频率；bound：波特率
+返回  值：无
+**************************************************************************/
+void uart_init(u32 pclk2,u32 bound)
+{  	 
+	float temp;
+	u16 mantissa;
+	u16 fraction;	   
+	temp=(float)(pclk2*1000000)/(bound*16);//得到USARTDIV
+	mantissa=temp;				 //得到整数部分
+	fraction=(temp-mantissa)*16; //得到小数部分	 
+  mantissa<<=4;
+	mantissa+=fraction; 
+	RCC->APB2ENR|=1<<2;   //使能PORTA口时钟  
+	RCC->APB2ENR|=1<<14;  //使能串口时钟 
+	GPIOA->CRH&=0XFFFFF00F;//IO状态设置
+	GPIOA->CRH|=0X000008B0;//IO状态设置
+		  
+	RCC->APB2RSTR|=1<<14;   //复位串口1
+	RCC->APB2RSTR&=~(1<<14);//停止复位	   	   
+	//波特率设置
+ 	USART1->BRR=mantissa; // 波特率设置	 
+	USART1->CR1|=0X200C;  //1位停止,无校验位.
+
+}
---
+++ SYSTEM/usart/usart.h
@@ -0,0 +1,42 @@
+/***********************************************
+公司：轮趣科技（东莞）有限公司
+品牌：WHEELTEC
+官网：wheeltec.net
+淘宝店铺：shop114407458.taobao.com 
+速卖通: https://minibalance.aliexpress.com/store/4455017
+版本：5.7
+修改时间：2021-04-29
+
+ 
+Brand: WHEELTEC
+Website: wheeltec.net
+Taobao shop: shop114407458.taobao.com 
+Aliexpress: https://minibalance.aliexpress.com/store/4455017
+Version:5.7
+Update：2021-04-29
+
+All rights reserved
+***********************************************/
+#ifndef __USART_H
+#define __USART_H
+#include "sys.h"
+#include "stdio.h"	 
+void usart1_send(u8 data);
+void uart_init(u32 pclk2,u32 bound);
+#endif	   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
